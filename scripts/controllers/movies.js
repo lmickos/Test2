f94747c14f3980a2fd56534b0c65def55dfc881e
Lars-Erik Mickos <mickos@mickos.se>
2017-01-08 12:19:23 +0100

    Added sort functionality

--- scripts/controllers/movies.js
+++ scripts/controllers/movies.js
@@ -2,7 +2,11 @@
 
 angular.module('movielist')
   .controller('MovielistCtrl', function($scope, MovieService) {
+    // Initialize default values
     $scope.movies = [];
+    $scope.orderByColumn = "Title";
+    $scope.direction = false;
+    // Load the movie data into memory
     MovieService.get(function(data) {
       $scope.movies = data.Movies;
     });
