41c8b57cccc9ba5c70ad8c5cdc27409acccd84ae
Lars-Erik Mickos <mickos@mickos.se>
2017-01-10 13:19:33 +0100

    Added multiple views support. Now has a main, a list and a table view. List view shows the data as a simple list.

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,18 @@
 angular.module('movielist',['ngResource','ngRoute', 'ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/movietable.html',
-        controller: 'MovielistCtrl',
-         })
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MovielistCtrl',
+     })
+    .when('/movietable', {
+      templateUrl: 'views/movietable.html',
+      controller: 'MovielistCtrl',
+    })
+    .when('/movielist', {
+      templateUrl: 'views/movielist.html',
+      controller: 'MovielistCtrl',
+    })
       .otherwise({
         redirectTo: '/'
 		 });
