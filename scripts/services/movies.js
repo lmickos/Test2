ce2bc0103aaa0a805eb84693e3fc95fe9a97a735
Lars-Erik Mickos <mickos@mickos.se>
2017-01-13 15:50:19 +0100

    Switched the REST emulation URL to a local reference in order to enable demo usage on an external site

--- scripts/services/movies.js
+++ scripts/services/movies.js
@@ -2,7 +2,9 @@
 
 angular.module('movielist')
   .factory('MovieService', function($resource) {
-    return $resource('/movies', {  }, {
+    <!-- Exchange the 'movies' reference below to a URL for a real rest service
+         This implementation uses a static local URL to emulate the REST data -->
+    return $resource('movies', {  }, {
       'get' : { method: 'GET'}
     });
   });
