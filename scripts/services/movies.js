2a81e198777384e86afb8c56e65ba69879bf4939
Lars-Erik Mickos <mickos@mickos.se>
2017-01-13 17:07:09 +0100

    Fix for a minor comment related bug

--- scripts/services/movies.js
+++ scripts/services/movies.js
@@ -2,8 +2,8 @@
 
 angular.module('movielist')
   .factory('MovieService', function($resource) {
-    <!-- Exchange the 'movies' reference below to a URL for a real rest service
-         This implementation uses a static local URL to emulate the REST data -->
+    /** Exchange the 'movies' reference below to a URL for a real rest service
+         This implementation uses a static local URL to emulate the REST data */
     return $resource('movies', {  }, {
       'get' : { method: 'GET'}
     });
